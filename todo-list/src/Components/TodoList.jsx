46811bad60b943d0347c7471c99d6d09b44351a7
maureen <maureen.amech123@gmail.com>
2025-07-19 16:44:54 +0100

    Add the main components of  TodoList

--- todo-list/src/Components/TodoList.jsx
+++ todo-list/src/Components/TodoList.jsx
@@ -3,9 +3,22 @@ import "./TodoList.css";
 
 const TodoList = () => {
   return (
-    <div></div>
-  )
-}
+    <>
+    <div className="todo-container">
+      <h1 className="title">My Todo List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Enter heading"
+          className="heading-input"
+         />
+         <button className="add-heading-button">Add Heading</button>
+      </div>
+    </div>
+    <div className="todo-main"></div>
+  </>
+  );
+};
 
 
 export default TodoList;
